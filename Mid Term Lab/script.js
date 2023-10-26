af30c83914215f480dfc8bad1e4f8f7f681d7324
Areeba Aleem <aribaaleem5@gmail.com>
2023-10-26 16:05:50 +0500

    mid_lab

---
+++ Mid Term Lab/script.js
@@ -0,0 +1,7 @@
+
+const logo = document.getElementById("logo");
+const mainMenu = document.getElementById("nav3");
+
+logo.addEventListener("click", function() {
+    mainMenu.style.display = "none";
+});
