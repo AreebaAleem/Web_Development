05b92dd3b7b85599ad6c939d58a296851bed453e
Areeba Aleem <aribaaleem5@gmail.com>
2023-12-19 01:44:52 +0500

    updated

--- Lab Task Two/routes/admin/products.js
+++ Lab Task Two/routes/admin/products.js
@@ -19,22 +19,43 @@ router.post("/products/add", productValidator, async (req, res) => {
   res.redirect("/admin/products");
 });
 
-router.get('/products/edit/:id', async function(req, res) {
-  let product = await Product.findById(req.params.id);
-  res.render("admin/products/edit", {
-    layout: "adminlayout",
-    product,
-  });
-  
-});
+router.get('/products/edit/:id', async function (req, res) {
+  try {
+    const product = await Product.findById(req.params.id);
 
+    if (!product) {
+      req.session.flash = { type: "danger", message: "Product not found" };
+      return res.redirect("/admin/products");
+    }
+
+    res.render("admin/products/edit", {
+      layout: "adminlayout",
+      product,
+    });
+  } catch (error) {
+    console.error(error);
+    req.session.flash = { type: "danger", message: "Error fetching product for edit" };
+    res.redirect("/admin/products");
+  }
+});
 
 router.post("/products/edit/:id", productValidator, async (req, res) => {
-  let product = await Product.findByIdAndUpdate(req.params.id, req.body);
-  req.session.flash = { type: "success", message: "Product Updated!" };
-  res.redirect("/admin/products");
-});
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body);
 
+    if (!product) {
+      req.session.flash = { type: "danger", message: "Product not found for update" };
+      return res.redirect("/admin/products");
+    }
+
+    req.session.flash = { type: "success", message: "Product Updated!" };
+    res.redirect("/admin/products");
+  } catch (error) {
+    console.error(error);
+    req.session.flash = { type: "danger", message: "Error updating product" };
+    res.redirect("/admin/products");
+  }
+});
 
 router.get("/products/delete/:id", async (req, res) => {
   let product = await Product.findByIdAndDelete(req.params.id);
